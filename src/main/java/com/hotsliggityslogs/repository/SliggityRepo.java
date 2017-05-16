73db69fb8db18626d0f9f3311e9e07f24ad9fd29
Ken Little <klittle@shelterinsurance.com>
2017-05-16 10:43:19 -0500

    mongo repo

---
+++ src/main/java/com/hotsliggityslogs/repository/SliggityRepo.java
@@ -0,0 +1,7 @@
+package com.hotsliggityslogs.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SliggityRepo extends MongoRepository {}
