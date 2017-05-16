6ba04ae30f894c6ddec2a61fae776216e248dc2e
Ken Little <klittle@shelterinsurance.com>
2017-05-16 13:22:54 -0500

    add type

--- src/main/java/com/hotsliggityslogs/repository/SliggityRepo.java
+++ src/main/java/com/hotsliggityslogs/repository/SliggityRepo.java
@@ -1,7 +1,8 @@
 package com.hotsliggityslogs.repository;
 
+import com.hotsliggityslogs.models.Match;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SliggityRepo extends MongoRepository {}
+public interface SliggityRepo extends MongoRepository<Match, String> {}
